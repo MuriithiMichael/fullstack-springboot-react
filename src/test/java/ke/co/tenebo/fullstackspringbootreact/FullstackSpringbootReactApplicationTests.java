899100b05ccc0b7bbba00bd43afc81cb35ee2847
muriithimichael <muriithi91@gmail.com>
2022-01-30 14:56:28 +0300

    make test fail

--- src/test/java/ke/co/tenebo/fullstackspringbootreact/FullstackSpringbootReactApplicationTests.java
+++ src/test/java/ke/co/tenebo/fullstackspringbootreact/FullstackSpringbootReactApplicationTests.java
@@ -1,5 +1,6 @@
 package ke.co.tenebo.fullstackspringbootreact;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class FullstackSpringbootReactApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Oops test failed");
 	}
 
 }
